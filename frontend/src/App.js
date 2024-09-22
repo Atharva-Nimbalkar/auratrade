31eac93e2dd3659d3fa18880a0dfadb6f226eafe
Atharva Nimbalkar <g.brain2753@gmail.com>
2024-09-22 23:04:15 +0530

    header n footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import {Container} from 'react-bootstrap'
 import React from 'react'
-
-export const App = () => {
+import Header from './components/Header';
+import Footer from './components/Footer';
+const App = () => {
   return (
-    <div>welcome</div>
+    <>
+    <Header/>
+    <main className='py-3'>
+      <Container>
+        <h1>Welcome to AuraTrade</h1>
+      </Container>
+    </main> 
+    <Footer/>
+    </>
   )
 }
 
-export default App
+export default App;
