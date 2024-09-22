37c35f6a4d058249431916e5124e12c66f4ea29f
Atharva Nimbalkar <g.brain2753@gmail.com>
2024-09-23 00:35:10 +0530

    prducts are listed,Product component added in homescreen

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Row,Col} from 'react-bootstrap'
+import Product from '../components/Product'
+import products from '../products'
+const HomeScreen = () => {
+  return (
+    <>
+        <h1>
+            Latest Products
+        </h1>
+        <Row>
+            {products.map((product)=>(
+/* In this case, the column will take up 12 columns on small screens, 6 columns on
+medium screens, 4 columns on large screens, and 3 columns on extra-large screens. */
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product} />
+                    </Col>
+            ))}
+        </Row>
+    </>
+  )
+}
+
+export default HomeScreen
