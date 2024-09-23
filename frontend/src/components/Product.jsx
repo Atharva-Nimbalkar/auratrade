6affe85979b89ca32e51bb2bc5da59fb82fe09a8
Atharva Nimbalkar <g.brain2753@gmail.com>
2024-09-23 17:03:25 +0530

    react-router,router-bootstrap,LinkContainer added for path config.

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import {Card} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 const Product = ({product}) => {
   return (
     <Card className="my-3 p-3 rounded">
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
 {/* The line `<Card.Img src={product.image} variant="top"/>` is rendering an image inside a Bootstrap
 Card component. It sets the `src` attribute of the image to the value of `product.image`, which is
 likely a URL pointing to the image file. The `variant="top"` prop is specifying the placement of the
 image within the Card component, in this case, at the top.  */}
             <Card.Img src={product.image} variant="top"/>
-        </a>
+        </Link>
 
         <Card.Body>
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Title as="div">
                     <strong>{product.name}</strong>
                 </Card.Title>
-            </a>
+            </Link>
            {/* The `<Card.Text as="h3">` element is rendering a text element within a Bootstrap Card
            component. The `as="h3"` prop is specifying that the text should be rendered as an HTML
            `<h3>` element, which is a heading element with a specific semantic meaning in HTML.  */}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/HomeScreen';
+
+/* The `const router=createBrowserRouter()` creating a router instance using the
+`createBrowserRouter`. This function is typically used to create a router object that can be used to define the routing configuration for a
+React application. */
+const router=createBrowserRouter(
+  /* `createRoutesFromElements(` is a function that is used to create route configurations from React
+  elements.The function takes React elements as input and generates route configurations based on those elements. */
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route index={true} path='/' element={<HomeScreen/>}/>
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+ {/* `<RouterProvider router={router}/>`. It takes the `router` object created using
+`createBrowserRouter()` and makes it available to the entire application as a context. This allows
+components within the application to access and utilize the routing configuration defined by the
+`router` object. */ }
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
