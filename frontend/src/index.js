b4d23a786c9dfb5852e3b92b14114f7d189d8b67
Atharva Nimbalkar <g.brain2753@gmail.com>
2024-10-17 01:36:32 +0530

    cardScrren functionality added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,7 +15,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-
+import CartScreen from './screens/CartScreen';
 /* The `const router=createBrowserRouter()` creating a router instance using the
 `createBrowserRouter`. This function is typically used to create a router object that can be used to define the routing configuration for a
 React application. */
@@ -26,6 +26,7 @@ const router=createBrowserRouter(
     <Route path='/' element={<App/>}>
       <Route index={true} path='/' element={<HomeScreen/>}/>
       <Route path='/product/:id' element={<ProductScreen/>}/>
+      <Route path='/cart' element={<CartScreen/>}/>
     </Route>
   )
 )
