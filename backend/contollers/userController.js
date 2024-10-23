922d21af7bfc5d3a1d1b1c56bd0fe9766beaa959
Atharva Nimbalkar <g.brain2753@gmail.com>
2024-10-23 14:53:01 +0530

    user register endpoint added and encryption

--- backend/contollers/userController.js
+++ backend/contollers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
-import jwt from 'jsonwebtoken';
+import generateToken from '../utils/generateToken.js';
 
 //@desc description Auth user & get token login user
 // @route POST/api/users/login
@@ -13,18 +13,8 @@ const authUser=asyncHandler(async(req,res)=>{
 
 
     if(user &&  (await user.matchPassword(password))){
-
-        const token=jwt.sign({userId: user._id},process.env.JWT_SECRET,{
-            expiresIn : '30d',
-        });
-
-        //SET JWT as HTTP-only cookie
-        res.cookie('jwt',token,{
-            httpOnly:true,
-            secure: process.env.NODE_ENV!=='development',
-            sameSite : 'strict',
-            maxAge: 30*24*60*60*1000, //30 days
-        });
+        generateToken(res,user._id);
+        
         res.json({
             _id: user._id,
             name: user.name,
@@ -46,7 +36,34 @@ const authUser=asyncHandler(async(req,res)=>{
 //@router POST/api/users
 //@access Public
 const registerUser=asyncHandler(async(req,res)=>{
-    res.send('register user');
+    // res.send('register user');
+
+    const {name,email,password}=req.body;
+
+    const userExists=await User.findOne({email});
+
+    if(userExists){//if user already exists
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user=await User.create({
+        name,email,password
+    });
+
+    if(user){
+        generateToken(res,user._id);
+
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            isAdmin:user.isAdmin,
+        });
+    }else{
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
 });
 
 //@desc logout user/clear cookie
@@ -54,6 +71,7 @@ const registerUser=asyncHandler(async(req,res)=>{
 //@access Private
 const logoutUser=asyncHandler(async(req,res)=>{
     // res.send('logout user');
+    // sets a cookie named 'jwt' with an empty value and specific options to make it HTTP-only and expired.
     res.cookie('jwt','',{
         httpOnly:true,
         expires:new Date(0)
