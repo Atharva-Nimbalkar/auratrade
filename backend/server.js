2e16ccf6d90d00af6628d0cfd6b6221b70839fb5
Atharva Nimbalkar <g.brain2753@gmail.com>
2024-11-01 16:16:25 +0530

    order controllers functions rooted inside routes

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import connectDB from './config/db.js';
 // import { notFound,errorHandler } from './middleware/errorMiddleware.js'; //error handling done in productContoller.js file
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 const port=process.env.PORT || 8000;
 
@@ -37,7 +38,7 @@ app.get('/',(req,res)=>{
 
 app.use('/api/products',productRoutes);
 app.use('/api/users',userRoutes);
-
+app.use('/api/orders',orderRoutes);
 
 // app.use(notFound);
 // app.use(errorHandler);
