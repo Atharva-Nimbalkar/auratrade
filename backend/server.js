f10dc004ab4d7174881d5721e30984bca37b3d9a
Atharva Nimbalkar <g.brain2753@gmail.com>
2024-11-07 11:42:33 +0530

    updateOrderToPaid updated and paypal setup done

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,9 @@ app.get('/',(req,res)=>{
 //     res.json(product);
 // })
 
+app.get('/api/config/paypal',(req,res)=>{
+    res.send({clientId: process.env.PAYPAL_CLIENT_ID});
+})
 app.use('/api/products',productRoutes);
 app.use('/api/users',userRoutes);
 app.use('/api/orders',orderRoutes);
