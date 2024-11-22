86f24a815ad2fc1e0bc65446d62d93a2c1e191e1
Atharva Nimbalkar <g.brain2753@gmail.com>
2024-11-22 20:53:48 +0530

    orderlist for admin only created

---
+++ frontend/src/components/AdminRoute.jsx
@@ -0,0 +1,14 @@
+import { Outlet,Navigate } from "react-router-dom"
+import {useSelector} from "react-redux";
+
+
+const AdminRoute = () => {
+    const {userInfo}=useSelector((state)=>state.auth);
+
+//The PrivateRoute component is used to protect routes that should only be accessible to authenticated users
+    return userInfo && userInfo.isAdmin ? 
+    (<Outlet/>) : 
+    (<Navigate to="/login"  replace />);// The replace attribute ensures that the navigation replaces the current entry in the history stack, preventing the user from navigating back to the protected route using the browser's back button
+} 
+
+export default AdminRoute;
